a135ddad516cff53a6c841f87a48b52471a49f5e
Ogheneyoma <Lawrenceyoma@gmail.com>
2024-12-17 02:06:29 +0100

    first commit

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+// fn main() {
+//     // println!("Hello, world!");
+//     let x = 5;
+//     let y = -15;
+//     let _z: u32 = 1000; 
+
+//     let _float_number = 1000.004;
+
+//     print!("x: {}, y: {}", x, y);
+// }
+
+fn main(){
+    // let is_male = false;
+    // let is_above_18 = true;
+
+    // if is_male {
+    //     println!("You are a guy");
+    // }else{
+    //     println!("You are not a guy");
+    // }
+
+    // if is_male && is_above_18 {
+    //     println!("You are a legal male")
+    // }
+
+    let greeting = String::from("hello world");
+    println!("{}", greeting);
+}
